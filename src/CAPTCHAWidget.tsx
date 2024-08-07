1f59197a01d0ce4adeff749b13d3ab9ad7b25038
Tomon <t.ukowski@gmail.com>
2024-08-07 14:28:34 +0200

    part 2

--- src/CAPTCHAWidget.tsx
+++ src/CAPTCHAWidget.tsx
@@ -264,7 +264,7 @@ const CAPTCHAWidget: React.FC<CAPTCHAWidgetProps> = ({ onSolve }) => {
           <CAPTCHAButton onClick={getPositions}>Pozycje puzzli</CAPTCHAButton>
           <CAPTCHAButton onClick={handleSolveClick}>Zatwierdź</CAPTCHAButton>
         </div>
-        <div>ver 0.6.11</div>
+        <div>ver 0.6.13</div>
       </CAPTCHAContainer>
     );
   }
